50e56d6ce9fd8ea5d32bc1a2d2cff723494a61c2
Elizabeth Flournoy <bflournoy@gmail.com>
2017-04-23 18:20:26 -0600

    fully functioning site. Needs style updates

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 
   $(document).ready(function(){
       console.log("poop");
+      $('.button-collapse').sideNav();
       $('.parallax').parallax();
     });
--- js/order.js
+++ js/order.js
@@ -6,24 +6,38 @@ var $cartBody = $('#cart').children('tbody');
 var $subtotal = $('#subtotalAmt').text();
 var $tax = $('#taxAmt').text();
 var $total = $('#totalAmt').text();
-console.log($total);
+var subtotal = 0;
+var tax = 0;
+var total = 0;
+var taxrate = .0885
 
 for(let i = 0; i<$deck.length; i++){
   var $orderButton = $($deck[i]).children('.card-action');
 
   $orderButton.click('a', function(){
     var $itemName = $($deck[i]).children('.card-content').children('h5').text();
-    var $itemPrice = $($deck[i]).children('.card-content').children('p').text();
+    var $itemPrice = $($deck[i]).children('.card-content').children('p').children('span').text();
+
     // insert items in to order cart
     var $row = $('<tr>');
     var $itemCell = $('<td>').text($itemName);
     var $priceCell = $('<td>').text($itemPrice);
     $row.append($itemCell, $itemPrice);
     $cartBody.append($row);
-    // update totals section
+
+    // update subtotals
+    subtotal += parseFloat($itemPrice);
+    $('#subtotalAmt').text('$'+subtotal);
+
+    // update tax
+    tax += taxrate*parseFloat(subtotal);
+    $('#taxAmt').text('$'+tax.toFixed(2));
+
+    // update total
+    total = parseFloat(subtotal+tax);
+    $('#totalAmt').text('$'+total.toFixed(2));
 
   })
-
 }
 
 

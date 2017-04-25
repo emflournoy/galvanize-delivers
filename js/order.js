dfa17aee874ce5be9326394b80c8cb2c00ea9b33
Elizabeth Flournoy <bflournoy@gmail.com>
2017-04-25 17:37:23 -0600

    working site

--- js/order.js
+++ js/order.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
 console.log('poop2');
 
+$('.modal').modal();
+$('.button-collapse').sideNav();
+
+//variables for ordering
 var $deck = $('.card');
 var $cartBody = $('#cart').children('tbody');
 var $subtotal = $('#subtotalAmt').text();
@@ -11,6 +15,7 @@ var tax = 0;
 var total = 0;
 var taxrate = .0885
 
+//ordering function
 for(let i = 0; i<$deck.length; i++){
   var $orderButton = $($deck[i]).children('.card-action');
 
@@ -22,15 +27,16 @@ for(let i = 0; i<$deck.length; i++){
     var $row = $('<tr>');
     var $itemCell = $('<td>').text($itemName);
     var $priceCell = $('<td>').text($itemPrice);
+    $priceCell.css('text-align : center');
     $row.append($itemCell, $itemPrice);
     $cartBody.append($row);
 
     // update subtotals
     subtotal += parseFloat($itemPrice);
-    $('#subtotalAmt').text('$'+subtotal);
+    $('#subtotalAmt').text('$'+subtotal.toFixed(2));
 
     // update tax
-    tax += taxrate*parseFloat(subtotal);
+    tax = taxrate*parseFloat(subtotal);
     $('#taxAmt').text('$'+tax.toFixed(2));
 
     // update total
@@ -40,14 +46,35 @@ for(let i = 0; i<$deck.length; i++){
   })
 }
 
+// variables for submitting order
+var $submitButton = $('#submitButton');
+var $firstName = $('#first_name');
+var $lastName = $('#last_name');
+var $telephone = $('#telephone');
+var $address1 = $('#address1');
+var $address2 = $('#address2');
+var $email = $('#email');
 
 
 
-
-// $deck.click('a', function(event){
-//   var $clickedCard = event.target.innerText
-//   console.log($clickedCard);
-// })
+//submitting order function
+$submitButton.click('button', function(event){
+  event.preventDefault();
+  if($lastName.val() === '' ||
+    $firstName.val() === '' ||
+    $telephone.val() === '' ||
+    $address1.val() === '' ||
+    $address2.val() === '' ||
+    $email.val() === '' ||
+    subtotal === 0
+  ){
+    console.log('incomplete form');
+    Materialize.toast('Way to fill out the form wrong. Try again.', 4000)
+  } else {
+    Materialize.toast("Success. It's going down.", 4000)
+    console.log('complete form');
+  }
+})
 
 
 });
